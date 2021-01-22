9314f9e34c4ae0f7301a5116a3f8947ff1467c73
Jorge Alberto Cricelli <jacricelli@gmail.com>
2021-01-21 23:32:14 -0300

    Permitir una única instancia de la aplicación

--- Program.cs
+++ Program.cs
@@ -1,22 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-
-namespace HikDownloader
+﻿namespace HikDownloader
 {
-    static class Program
+    using System;
+    using System.Threading;
+    using System.Windows.Forms;
+
+    public static class Program
     {
         /// <summary>
         /// Punto de entrada principal para la aplicación.
         /// </summary>
         [STAThread]
-        static void Main()
+        public static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MainWindow());
+            using (Mutex mutex = new Mutex(false, "Global\\93e38211-85fd-4c62-9fb9-6e17b9d82580"))
+            {
+                if (!mutex.WaitOne(0, false))
+                {
+                    MessageBox.Show("Ya hay una instancia de la aplicación en ejecución.", Application.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                    return;
+                }
+
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new MainWindow());
+            }
         }
     }
 }
