bf4516edbe548fbfada2906257dde034b730a4a4
Jorge Alberto Cricelli <jacricelli@gmail.com>
2021-01-23 22:28:30 -0300

    Corregir tamaño

--- MainWindow.Designer.cs
+++ MainWindow.Designer.cs
@@ -484,7 +484,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1056, 406);
+            this.ClientSize = new System.Drawing.Size(1053, 402);
             this.Controls.Add(this.groupBox3);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
