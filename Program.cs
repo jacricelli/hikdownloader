6eff3b0833f890ae5f0442fe6fa3c1928aa94c1d
Jorge Alberto Cricelli <jacricelli@gmail.com>
2021-01-25 15:24:03 -0300

    Revert "Permitir una única instancia de la aplicación"
    This reverts commit 9314f9e34c4ae0f7301a5116a3f8947ff1467c73.

--- Program.cs
+++ Program.cs
@@ -1,30 +1,19 @@
 ﻿namespace HikDownloader
 {
     using System;
-    using System.Threading;
     using System.Windows.Forms;
 
-    public static class Program
+    static class Program
     {
         /// <summary>
         /// Punto de entrada principal para la aplicación.
         /// </summary>
         [STAThread]
-        public static void Main()
+        static void Main()
         {
-            using (Mutex mutex = new Mutex(false, "Global\\93e38211-85fd-4c62-9fb9-6e17b9d82580"))
-            {
-                if (!mutex.WaitOne(0, false))
-                {
-                    MessageBox.Show("Ya hay una instancia de la aplicación en ejecución.", Application.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Error);
-
-                    return;
-                }
-
-                Application.EnableVisualStyles();
-                Application.SetCompatibleTextRenderingDefault(false);
-                Application.Run(new MainWindow());
-            }
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MainWindow());
         }
     }
 }
