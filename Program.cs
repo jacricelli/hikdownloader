33032e8504aa2836e3cee438f5b008f9c7a9195f
Jorge Alberto Cricelli <jacricelli@gmail.com>
2021-01-27 14:21:27 -0300

    Agregar bloque de documentación y especificar visibilidad

--- Program.cs
+++ Program.cs
@@ -3,13 +3,16 @@
     using System;
     using System.Windows.Forms;
 
-    static class Program
+    /// <summary>
+    /// Programa.
+    /// </summary>
+    public static class Program
     {
         /// <summary>
         /// Punto de entrada principal para la aplicación.
         /// </summary>
         [STAThread]
-        static void Main()
+        public static void Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
