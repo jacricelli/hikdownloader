b82f8c55e162e55bcd9a02ba6c7cb07cd8e3007c
Jorge Alberto Cricelli <jacricelli@gmail.com>
2021-02-13 15:04:59 -0300

    Agregar método GetPath

---
+++ Util.cs
@@ -0,0 +1,35 @@
+﻿namespace HikDownloader
+{
+    using System.IO;
+    using System.Reflection;
+
+    /// <summary>
+    /// Colección de métodos utilizados por la aplicación.
+    /// </summary>
+    public static class Util
+    {
+        /// <summary>
+        /// Obtiene la ruta de acceso al directorio desde donde se ejecuta la aplicación.
+        /// </summary>
+        /// <param name="subdir">Subdirectorio a anexar a la ruta.</param>
+        /// <param name="file">Archivo a anexar a la ruta.</param>
+        /// <returns>Ruta de acceso al directorio, subdirectorio o archivo.</returns>
+        public static string GetPath(string subdir = "", string file = "")
+        {
+            var path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
+            if (subdir != string.Empty)
+            {
+                path += "\\" + subdir;
+                Directory.CreateDirectory(path);
+            }
+
+            if (file != string.Empty)
+            {
+                path += "\\" + file;
+            }
+
+            return path;
+        }
+    }
+}
