da56777e31a4a0b01b55a6a0d16c950156389676
Jorge Alberto Cricelli <jacricelli@gmail.com>
2021-02-13 19:18:45 -0300

    Agregar método para obtener la descripción del último error

--- HCNetSDK.cs
+++ HCNetSDK.cs
@@ -1,5 +1,8 @@
 ﻿namespace HikDownloader
 {
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Reflection;
     using System.Runtime.InteropServices;
 
     /// <summary>
@@ -8,9 +11,31 @@
     public static class HCNetSDK
     {
         /// <summary>
+        /// La descripción de los errores de HCNetSDK.
+        /// </summary>
+        private static Dictionary<uint, string> _errors = new Dictionary<uint, string>();
+
+        /// <summary>
         /// Obtiene un valor que indica si se ha inicializado el entorno de programación.
         /// </summary>
         public static bool IsInitialized { get; private set; }
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        static HCNetSDK()
+        {
+            var resource = Assembly.GetExecutingAssembly().GetManifestResourceStream("HikDownloader.Resources.HCNetSDKErrors.txt");
+            using (var reader = new StreamReader(resource))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    var error = line.Split('\t');
+                    _errors.Add(uint.Parse(error[0]), error[1]);
+                }
+            }
+        }
 
         /// <summary>
         /// Inicializa el entorno de programación.
@@ -45,6 +70,28 @@
         public static bool SetLogToFile(HCNetSDKLogConfig config)
         {
             return NET_DVR_SetLogToFile(config.Level, config.Dir, config.AutoDel);
+        }
+
+        /// <summary>
+        /// Obtiene el código de error generado por la última operación.
+        /// </summary>
+        /// <returns>El código de error de la última operación.</returns>
+        [DllImport(@"lib\HCNetSDK.dll", EntryPoint = "NET_DVR_GetLastError")]
+        public static extern uint GetLastErrno();
+
+        /// <summary>
+        /// Obtiene la descripción del error generado por la última operación.
+        /// </summary>
+        /// <returns>La descripción del error de la última operación.</returns>
+        public static string GetLastError()
+        {
+            var errno = GetLastErrno();
+            if (_errors.ContainsKey(errno))
+            {
+                return _errors[errno];
+            }
+
+            return $"El código de error '{errno}' no se encuentra documentado.";
         }
 
         /// <summary>
