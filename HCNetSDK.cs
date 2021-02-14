cfa55ac907a41ea47ceda33f7403ae45bc1b022a
Jorge Alberto Cricelli <jacricelli@gmail.com>
2021-02-14 00:17:18 -0300

    Implementar la búsqueda de grabaciones

--- HCNetSDK.cs
+++ HCNetSDK.cs
@@ -1,5 +1,6 @@
 ﻿namespace HikDownloader
 {
+    using System;
     using System.Collections.Generic;
     using System.IO;
     using System.Reflection;
@@ -312,6 +313,348 @@
 
             return $"El código de error '{errno}' no se encuentra documentado.";
         }
+        #endregion
+
+        #region Búsqueda
+        /// <summary>
+        /// Realiza una búsqueda.
+        /// </summary>
+        /// <param name="channels">Uno o más canales.</param>
+        /// <param name="start">Fecha de comienzo.</param>
+        /// <param name="end">Fecha de finalización.</param>
+        /// <returns>Resultados de la búsqueda.</returns>
+        public static List<NET_DVR_FINDDATA_V30> Search(int[] channels, DateTime start, DateTime end)
+        {
+            var results = new List<NET_DVR_FINDDATA_V30>();
+
+            foreach (var channel in channels)
+            {
+                foreach (var from in EachDay(start, end))
+                {
+                    var thru = from.AddHours(23).AddMinutes(59).AddSeconds(59);
+                    if (thru > end)
+                    {
+                        thru = end;
+                    }
+
+                    var conditions = default(NET_DVR_FILECOND_V40);
+                    conditions.lChannel = channel;
+                    conditions.dwFileType = 0xff;
+                    conditions.dwIsLocked = 0xff;
+
+                    conditions.struStartTime.dwYear = (uint)from.Year;
+                    conditions.struStartTime.dwMonth = (uint)from.Month;
+                    conditions.struStartTime.dwDay = (uint)from.Day;
+                    conditions.struStartTime.dwHour = 0;
+                    conditions.struStartTime.dwMinute = 0;
+                    conditions.struStartTime.dwSecond = 0;
+
+                    conditions.struStopTime.dwYear = (uint)thru.Year;
+                    conditions.struStopTime.dwMonth = (uint)thru.Month;
+                    conditions.struStopTime.dwDay = (uint)thru.Day;
+                    conditions.struStopTime.dwHour = 23;
+                    conditions.struStopTime.dwMinute = 59;
+                    conditions.struStopTime.dwSecond = 59;
+
+                    var handle = NET_DVR_FindFile_V40(_userId, ref conditions);
+                    if (handle > -1)
+                    {
+                        var record = default(NET_DVR_FINDDATA_V30);
+                        while (true)
+                        {
+                            var result = NET_DVR_FindNextFile_V30(handle, ref record);
+                            if (result == NET_DVR_ISFINDING)
+                            {
+                                continue;
+                            }
+                            else if (result == NET_DVR_FILE_SUCCESS)
+                            {
+                                if (!results.Contains(record))
+                                {
+                                    results.Add(record);
+                                }
+                            }
+                            else if (result == NET_DVR_FIND_TIMEOUT || result == NET_DVR_FILE_NOFIND || result == NET_DVR_NOMOREFILE || result == NET_DVR_FILE_EXCEPTION)
+                            {
+                                break;
+                            }
+                        }
+
+                        NET_DVR_FindClose_V30(handle);
+                    }
+                    else
+                    {
+                        throw new Exception(GetLastError());
+                    }
+                }
+            }
+
+            return results;
+        }
+
+        /// <summary>
+        /// Obtiene cada día entre dos fechas.
+        /// </summary>
+        /// <param name="from">Desde.</param>
+        /// <param name="thru">Hasta</param>
+        /// <returns>Cada día entre <paramref name="from"/> y <paramref name="thru"/> inclusive.</returns>
+        private static IEnumerable<DateTime> EachDay(DateTime from, DateTime thru)
+        {
+            for (var day = from.Date; day.Date <= thru.Date; day = day.AddDays(1))
+                yield return day;
+        }
+
+        /// <summary>
+        /// Longitud para el dato GUID.
+        /// </summary>
+        private const int GUID_LEN = 16;
+
+        /// <summary>
+        /// Longitud del número de tarjeta para exterior.
+        /// </summary>
+        private const int CARDNUM_LEN_OUT = 32;
+
+        /// <summary>
+        /// Búsqueda completada.
+        /// </summary>
+        private const int NET_DVR_FILE_SUCCESS = 1000;
+
+        /// <summary>
+        /// No se ha encontrado el archivo.
+        /// </summary>
+        private const int NET_DVR_FILE_NOFIND = 1001;
+
+        /// <summary>
+        /// Búsqueda en progreso.
+        /// </summary>
+        private const int NET_DVR_ISFINDING = 1002;
+
+        /// <summary>
+        /// No hay más archivos.
+        /// </summary>
+        private const int NET_DVR_NOMOREFILE = 1003;
+
+        /// <summary>
+        /// Error en la búsqueda.
+        /// </summary>
+        private const int NET_DVR_FILE_EXCEPTION = 1004;
+
+        /// <summary>
+        /// Tiempo de espera agotado.
+        /// </summary>
+        private const int NET_DVR_FIND_TIMEOUT = 10005;
+
+        /// <summary>
+        /// Condiciones de búsqueda.
+        /// </summary>
+        [StructLayoutAttribute(LayoutKind.Sequential)]
+        public struct NET_DVR_FILECOND_V40
+        {
+            /// <summary>
+            /// Número de canal.
+            /// </summary>
+            public int lChannel;
+
+            /// <summary>
+            /// Tipo de archivo.
+            /// </summary>
+            public uint dwFileType;
+
+            /// <summary>
+            /// Indica si el archivo está bloqueado.
+            /// </summary>
+            public uint dwIsLocked;
+
+            /// <summary>
+            /// Indica si se utiliza número de tarjeta.
+            /// </summary>
+            public uint dwUseCardNo;
+
+            /// <summary>
+            /// Número de tarjeta.
+            /// </summary>
+            [MarshalAsAttribute(UnmanagedType.ByValArray, SizeConst = CARDNUM_LEN_OUT, ArraySubType = UnmanagedType.I1)]
+            public byte[] sCardNumber;
+
+            /// <summary>
+            /// Fecha de comienzo.
+            /// </summary>
+            public NET_DVR_TIME struStartTime;
+
+            /// <summary>
+            /// Fecha de finalización.
+            /// </summary>
+            public NET_DVR_TIME struStopTime;
+
+            /// <summary>
+            /// Indica si se extrae el cuadro.
+            /// </summary>
+            public byte byDrawFrame;
+
+            /// <summary>
+            /// Tipo de búsqueda.
+            /// </summary>
+            public byte byFindType;
+
+            /// <summary>
+            /// Indica si se realiza una búsqueda rápida.
+            /// </summary>
+            public byte byQuickSearch;
+
+            /// <summary>
+            /// Indica si se realiza una búsqueda especial.
+            /// </summary>
+            public byte bySpecialFindInfoType;
+
+            /// <summary>
+            /// Número de volumen.
+            /// </summary>
+            public uint dwVolumeNum;
+
+            /// <summary>
+            /// GUID.
+            /// </summary>
+            [MarshalAsAttribute(UnmanagedType.ByValArray, SizeConst = GUID_LEN, ArraySubType = UnmanagedType.I1)]
+            public byte[] byWorkingDeviceGUID;
+
+            /// <summary>
+            /// Unión de la condición de búsqueda.
+            /// </summary>
+            public NET_DVR_SPECIAL_FINDINFO_UNION uSpecialFindInfo;
+
+            /// <summary>
+            /// Reservado.
+            /// </summary>
+            [MarshalAsAttribute(UnmanagedType.ByValArray, SizeConst = 32, ArraySubType = UnmanagedType.I1)]
+            public byte[] byRes2;
+        }
+
+        /// <summary>
+        /// Resultado de búsqueda.
+        /// </summary>
+        [StructLayoutAttribute(LayoutKind.Sequential, CharSet = CharSet.Ansi)]
+        public struct NET_DVR_FINDDATA_V30
+        {
+            /// <summary>
+            /// Nombre del archivo.
+            /// </summary>
+            [MarshalAsAttribute(UnmanagedType.ByValTStr, SizeConst = 100)]
+            public string sFileName;
+
+            /// <summary>
+            /// Fecha de comienzo.
+            /// </summary>
+            public NET_DVR_TIME struStartTime;
+
+            /// <summary>
+            /// Fecha de finalización.
+            /// </summary>
+            public NET_DVR_TIME struStopTime;
+
+            /// <summary>
+            /// Tamaño del archivo.
+            /// </summary>
+            public uint dwFileSize;
+
+            /// <summary>
+            /// Número de tarjeta.
+            /// </summary>
+            [MarshalAsAttribute(UnmanagedType.ByValTStr, SizeConst = 32)]
+            public string sCardNum;
+
+            /// <summary>
+            /// Indica si el archivo está bloqueado.
+            /// </summary>
+            public byte byLocked;
+
+            /// <summary>
+            /// Tipo de archivo.
+            /// </summary>
+            public byte byFileType;
+
+            /// <summary>
+            /// Reservado.
+            /// </summary>
+            [MarshalAsAttribute(UnmanagedType.ByValArray, SizeConst = 2, ArraySubType = UnmanagedType.I1)]
+            public byte[] byRes;
+        }
+
+        /// <summary>
+        /// Estructura del parámetro de tiempo.
+        /// </summary>
+        [StructLayoutAttribute(LayoutKind.Sequential)]
+        public struct NET_DVR_TIME
+        {
+            /// <summary>
+            /// Año.
+            /// </summary>
+            public uint dwYear;
+
+            /// <summary>
+            /// Mes.
+            /// </summary>
+            public uint dwMonth;
+
+            /// <summary>
+            /// Día.
+            /// </summary>
+            public uint dwDay;
+
+            /// <summary>
+            /// Hora.
+            /// </summary>
+            public uint dwHour;
+
+            /// <summary>
+            /// Minuto.
+            /// </summary>
+            public uint dwMinute;
+
+            /// <summary>
+            /// Segundo.
+            /// </summary>
+            public uint dwSecond;
+        }
+
+        /// <summary>
+        /// Especifica la unión de la condición de búsqueda.
+        /// </summary>
+        [StructLayoutAttribute(LayoutKind.Explicit)]
+        public struct NET_DVR_SPECIAL_FINDINFO_UNION
+        {
+            /// <summary>
+            /// Tamaño de la unión.
+            /// </summary>
+            [FieldOffsetAttribute(0)]
+            [MarshalAsAttribute(UnmanagedType.ByValArray, SizeConst = 8, ArraySubType = UnmanagedType.I1)]
+            public byte[] byLenth;
+        }
+
+        /// <summary>
+        /// Lleva a cabo una búsqueda de archivos.
+        /// </summary>
+        /// <param name="lUserID">Identificador del usuario.</param>
+        /// <param name="pFindCond">Condiciones de búsqueda.</param>
+        /// <returns>Devuelve TRUE si la operación se ha completado exitosamente, FALSE de lo contrario.</returns>
+        [DllImport(@"lib\HCNetSDK.dll")]
+        private static extern int NET_DVR_FindFile_V40(int lUserID, ref NET_DVR_FILECOND_V40 pFindCond);
+
+        /// <summary>
+        /// Obtiene el próximo archivo de la búsqueda.
+        /// </summary>
+        /// <param name="lFindHandle">Identificador de la búsqueda.</param>
+        /// <param name="lpFindData">Datos de la búsqueda.</param>
+        /// <returns>Devuelve -1 en caso de error, otro valor que indica el estado de la búsqueda.</returns>
+        [DllImport(@"lib\HCNetSDK.dll")]
+        private static extern int NET_DVR_FindNextFile_V30(int lFindHandle, ref NET_DVR_FINDDATA_V30 lpFindData);
+
+        /// <summary>
+        /// Detiene una búsqueda.
+        /// </summary>
+        /// <param name="lFindHandle">Identificador de la búsqueda.</param>
+        /// <returns>Devuelve TRUE si la operación se ha completado exitosamente, FALSE de lo contrario.</returns>
+        [DllImport(@"lib\HCNetSDK.dll")]
+        private static extern bool NET_DVR_FindClose_V30(int lFindHandle);
         #endregion
     }
 }
